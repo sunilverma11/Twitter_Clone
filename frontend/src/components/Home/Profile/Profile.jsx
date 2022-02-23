c30d49dc8bc73510955b2965a25eb2f70cc62d7e
sunilverma11 <srksaroya@hotmail.com>
2022-02-24 00:30:56 +0530

    added in profile

--- frontend/src/components/Home/Profile/Profile.jsx
+++ frontend/src/components/Home/Profile/Profile.jsx
@@ -1,10 +1,37 @@
 import "./Profile.css"
-
+import ArrowBackSharpIcon from '@mui/icons-material/ArrowBackSharp';
 
 export const Profile = () => {
+let names="SUNIL KUMAR VERMA"
+  let type = "Tweets"
+  let userid = "Sunilraj_verma"
+  let joinDate="23/02/2022"
     return (
-        <div className="home">
-      Hello
+      <div className="profile">
+         
+        <div className="name_div">
+          
+          <span className="profile_back_arrow"><ArrowBackSharpIcon /></span>
+          <div>
+            <p className="profile_name">{names}</p>
+          <p>{50 } {type} </p>
+          </div>
+          
+        </div>
+        <div className="profile_background_div">
+            <img className="profile_background" src="https://pbs.twimg.com/profile_banners/424147534/1366933847/600x200" alt="" />
+        </div>
+        <div className="profile_pic_div">
+            <img className="profile_pic" src="https://pbs.twimg.com/profile_images/925429338920558592/EsmzEIJP_400x400.jpg" alt="" />
+            <div className="">
+            <p  className="profile_pic_text">{names}</p>
+          <p>@{userid} </p>
+          </div>
+        </div>
+        <div className="editprofile_btn">
+          <button>Edit Profile</button>
+        </div>
+        
         </div>
     )
     }
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,8 @@
 import Feed from "../../components/Home/Feed/Feed"
 import Sidebar from "../../components/Home/Sidebar/Sidebar"
 import Widgets from "../../components/Home/Widgets/Widgets"
-import { Temp } from "../../components/Home/Profile/temp"
+import { Profile } from "../../components/Home/Profile/Profile"
+
 import "./Home.css"
 
 export const Home = ()=>{
@@ -13,7 +14,7 @@ export const Home = ()=>{
 
             {/* feed */}
             {/* <Feed/> */}
-            <Temp/>
+          <Profile/>
             {/* widgets */}
             <Widgets/>
 
