0821ee2ff9a4e19990f32c5324a2b86799dc6273
jayesh khairnar <jayeshk425@gmail.com>
2022-02-23 17:03:28 +0530

    home page init

---
+++ frontend/src/components/Home/Feed/Feed.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import "./Feed.css"
+
+function Feed() {
+  return (
+    <div className='feed'>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+        <div>feed</div>
+    </div>
+  )
+}
+
+export default Feed
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,22 @@
+import Feed from "../../components/Home/Feed/Feed"
+import Sidebar from "../../components/Home/Sidebar/Sidebar"
+import Widgets from "../../components/Home/Widgets/Widgets"
+
+import "./Home.css"
+
+export const Home = ()=>{
+    return (
+        <div className="home">
+
+            {/* sidebar */}
+            <Sidebar></Sidebar>
+
+            {/* feed */}
+            <Feed/>
+
+            {/* widgets */}
+            <Widgets/>
+
+        </div>
+    )
+}
