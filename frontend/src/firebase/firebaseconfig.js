19465db4325777dd04106395f2a7cfaa7da180a3
Sudhanshu894 <sidharts894@gmail.com>
2022-02-24 14:40:44 +0530

    signup login feature init

---
+++ frontend/src/firebase/firebaseconfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBKXdMWU8T1h44D1YSkSOjOGiqZqzbh5KE",
+  authDomain: "twitter-clone-react-da415.firebaseapp.com",
+  projectId: "twitter-clone-react-da415",
+  storageBucket: "twitter-clone-react-da415.appspot.com",
+  messagingSenderId: "26801675345",
+  appId: "1:26801675345:web:d68abdb0ec9e4a66562c70"
+};
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
