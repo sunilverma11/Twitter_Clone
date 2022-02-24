5cab2975031d01b0565d23854b0a85d5fc956f85
sunilverma11 <srksaroya@hotmail.com>
2022-02-24 16:38:03 +0530

    profile update

--- frontend/src/components/Home/Profile/Profile.jsx
+++ frontend/src/components/Home/Profile/Profile.jsx
@@ -18,7 +18,7 @@ export const Profile = () => {
         <div>
           <p className="profile_name">{names}</p>
           <p>
-            {50} {type}{" "}
+            {50} {type}
           </p>
         </div>
       </div>
@@ -30,15 +30,11 @@ export const Profile = () => {
         />
       </div>
       <div className="profile_pic_div">
-        <div>
-          
-        </div>
         <img
           className="profile_pic"
-          src="https://pbs.twimg.com/profile_images/925429338920558592/EsmzEIJP_400x400.jpg"
+          src="https://www.whatsappprofiledpimages.com/wp-content/uploads/2021/08/Profile-Photo-Wallpaper.jpg"
           alt=""
         />
-
 
         <div className="">
           <p className="profile_pic_text">{names}</p>
@@ -48,12 +44,27 @@ export const Profile = () => {
       <div className="editprofile_btn">
         <button>Edit Profile</button>
       </div>
-
-      <div className="profile_place_div ">
+      <div className="profile_place_div">
         <LocationOnOutlinedIcon /> <p className="center_profile">{place} </p>
         <DateRangeRoundedIcon />
         <p className="center_profile">Joined {join_date} </p>
       </div>
+      <div className="profile_follwer_div">
+        <p>{50} Following</p>
+        <p>{50} Followers</p>
+      </div>
+      <div className="profile_post_header">
+        <div className="tweets">Tweets</div>
+        <div className="tweetsnreply">Tweets&Reply</div>
+        <div className="media">Media</div>
+        <div className="likes">Likes</div>
+      </div>
+      <div>hello</div>
+      <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{" "}
+      <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{" "}
+      <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>
+      <div>hello</div> <div>hello</div> <div>hello</div> <div>hello</div>{" "}
+      <div>hello</div> <div>hello</div> <div>hello</div>
     </div>
   );
 };
