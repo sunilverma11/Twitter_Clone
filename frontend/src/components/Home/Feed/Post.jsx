414f0d0edaa473c561c9944772bbb29825f8c552
Sudhanshu894 <sidharts894@gmail.com>
2022-02-27 14:36:29 +0530

    update post page

--- frontend/src/components/Home/Feed/Post.jsx
+++ frontend/src/components/Home/Feed/Post.jsx
@@ -1,12 +1,12 @@
 import {
-  collection,
-  deleteDoc,
-  doc,
-  onSnapshot,
-  orderBy,
-  query,
-  setDoc,
-} from "firebase/firestore";
+    collection,
+    deleteDoc,
+    doc,
+    onSnapshot,
+    orderBy,
+    query,
+    setDoc,
+  } from "firebase/firestore";
 import { Avatar } from '@mui/material'
 import { useEffect, useState } from "react";
 import "./Post.css"
@@ -42,26 +42,46 @@ export const Post = (props) => {
             ),
             (snapshot) => setComments(snapshot.docs)
           ),
-          (snapshot) => setComments(snapshot.docs)
-        ),
-      [database, props.id]
-    );
-    useEffect(
-      () =>
-        onSnapshot(collection(database, "posts", props.id, "likes"), (snapshot) =>
-          setLikes(snapshot.docs)
-        ),
-      [database, props.id]
-    );
-    useEffect(()=>{
+        [database, props.id]
+      );
+    
+      useEffect(
+        () =>
+          onSnapshot(collection(database, "posts", props.id, "likes"), (snapshot) =>
+            setLikes(snapshot.docs)
+          ),
+        [database, props.id]
+      );
+    
+      useEffect(()=>{
+          if (user.displayName !== ""){
+            setLiked(
+                likes.findIndex((like) => like.id === user.uid) !== -1
+              )
+          }else {
+            setLiked(
+                likes.findIndex((like) => like.id === user.localId) !== -1
+              )
+          }
+      },[likes])
+    
+      const likePost = async () => {
         if (user.displayName !== ""){
-          setLiked(
-              likes.findIndex((like) => like.id === user.uid) !== -1
-            )
+            if (liked) {
+                await deleteDoc(doc(database, "posts", props.id, "likes", user.uid));
+              } else {
+                await setDoc(doc(database, "posts", props.id, "likes", user.uid), {
+                  username: user.displayName,
+                });
+              }
         }else {
-          setLiked(
-              likes.findIndex((like) => like.id === user.localId) !== -1
-            )
+            if (liked) {
+                await deleteDoc(doc(database, "posts", props.id, "likes", user.localId));
+              } else {
+                await setDoc(doc(database, "posts", props.id, "likes", user.localId), {
+                  username: user.email.split("@")[0],
+                });
+              }
         }
       };
   return (
@@ -80,8 +100,6 @@ export const Post = (props) => {
                 <div className="post_options">
                     <MoreHorizIcon/>
               </div>
-              <div className="post_options">
-                  <MoreHorizIcon/>
             </div>
             {props.post?.image === undefined ? <>
             <div className="post_description">
