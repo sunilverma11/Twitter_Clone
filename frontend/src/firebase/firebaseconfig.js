d0785403d0d699ba03ccc01931c9db9dfbc686fb
jayesh khairnar <jayeshk425@gmail.com>
2022-02-27 16:41:50 +0530

    Update firebaseconfig.js

--- frontend/src/firebase/firebaseconfig.js
+++ frontend/src/firebase/firebaseconfig.js
@@ -3,23 +3,23 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 
-// const firebaseConfig = {
-//   apiKey: process.env.API_KEY,
-//   authDomain: process.env.AUTHDOMAIN,
-//   projectId: process.env.PROJECT_ID,
-//   storageBucket: process.env.STORAGE_BUCKET,
-//   messagingSenderId: process.env.SENDER_ID,
-//   appId: process.env.APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCxGhqk5X7ydO3jgsepklxsej8hcb-PlBk",
-  authDomain: "twitter-clone-a7323.firebaseapp.com",
-  projectId: "twitter-clone-a7323",
-  storageBucket: "twitter-clone-a7323.appspot.com",
-  messagingSenderId: "649855340920",
-  appId: "1:649855340920:web:ad762e2a33ccd7415ef88d"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID,
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCxGhqk5X7ydO3jgsepklxsej8hcb-PlBk",
+//   authDomain: "twitter-clone-a7323.firebaseapp.com",
+//   projectId: "twitter-clone-a7323",
+//   storageBucket: "twitter-clone-a7323.appspot.com",
+//   messagingSenderId: "649855340920",
+//   appId: "1:649855340920:web:ad762e2a33ccd7415ef88d"
+// };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
